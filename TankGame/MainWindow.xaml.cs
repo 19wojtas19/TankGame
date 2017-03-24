3f443ba5417009a543d46a827612060fca0065ef
19wojtas19 <19wojtas19@gmail.com>
2017-03-24 22:07:03 +0100

    0.0.2
    Added map class

--- TankGame/MainWindow.xaml.cs
+++ TankGame/MainWindow.xaml.cs
@@ -15,14 +15,14 @@ using System.Windows.Shapes;
 
 namespace TankGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Map MainMap = new Map();
+            MainMap.Draw(MainCanvas);
+
         }
     }
 }
